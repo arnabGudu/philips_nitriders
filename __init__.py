54e3df77f44fc6188323e73a1d29c1a13868321e
arnabGudu <arnabgudu.gs@gmail.com>
2020-11-22 19:44:03 +0530

    added

--- __init__.py
+++ __init__.py
@@ -2,11 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__license__ = 'MIT'
-__maintainer__ = ['Avinash Kori']
-__email__ = ['koriavinash1@gmail.com']
-
-
 import os
 import sys
 from time import gmtime, strftime
@@ -14,8 +9,7 @@ from os.path import expanduser
 home = expanduser("~")
 import json
 
-
-with open(os.path.join(home, ".DeepBrainSeg/config.json"), "r") as write_file:
+with open(os.path.join(home, "./config.json"), "r") as write_file:
     config = json.load(write_file)
 
 if config['DBS_ANTS']:
